56b22194b19d67a4fa44528afc00947483c0ceae
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-09 15:18:24 -0400

    Fixing type def for 'skip'

--- dist/tiny-strings.cjs
+++ dist/tiny-strings.cjs
@@ -3,7 +3,7 @@
  *
  * @copyright 2024 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 1.0.9
+ * @version 1.0.10
  */
 'use strict';
 
--- types/strings.d.ts
+++ types/strings.d.ts
@@ -1,1 +1,1 @@
-export function strings(arg: Record<string | number, any> | any[], keys?: boolean, skip?: [string | number]): string[];
+export function strings(arg: Record<string | number, any> | any[], keys?: boolean, skip?: string[] | number[] | [string | number]): string[];
